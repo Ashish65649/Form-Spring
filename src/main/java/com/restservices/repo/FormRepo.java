e389dc168c869f578a4763182ed23cfb15f9ecd8
Ashish Ranjan Kumar <imashish2589@gmail.com>
2021-07-21 15:33:23 +0530

    Initial Commit

--- src/main/java/com/restservices/repo/FormRepo.java
+++ src/main/java/com/restservices/repo/FormRepo.java
@@ -1,8 +1,8 @@
-package com.restservices.repo;
-
-import org.springframework.data.repository.CrudRepository;
-import com.restservices.controller.entities.Form;
-
-public interface FormRepo extends CrudRepository<Form, Integer> {
-	
-}
+//package com.restservices.repo;
+//
+//import org.springframework.data.repository.CrudRepository;
+//import com.restservices.controller.entities.Form;
+//
+//public interface FormRepo extends CrudRepository<Form, Integer> {
+//	
+//}
